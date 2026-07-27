bb7d68066da07a25385583aa8ef3a3e689f2c6b7
ludmilarykhlova-tech <ludmilarykhlova@gmail.com>
2026-07-27 04:22:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,5 +53,5 @@ async def main():
     print("Бот запущен!")
     await dp.start_polling(bot)
 
-if name == "main":
-    asyncio.run(main())
+    if __name__== "__main__":
+        asyncio.run(main())
